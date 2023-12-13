d339620820fb3df0f219efe9cc41bdc651c8051a
kristinwegener <kristinwegener94@gmail.com>
2023-12-13 16:13:11 +0100

    added design files

--- components/VolumeDetail.js
+++ components/VolumeDetail.js
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
+import styled from "styled-components";
+import { Lora } from "next/font/google";
 
 export default function VolumeDetail({ volume }) {
   return (
@@ -7,19 +9,29 @@ export default function VolumeDetail({ volume }) {
       <Link href="../../volumes">Back to Volumes</Link>
       <h1>{volume.title}</h1>
       <p>{volume.description}</p>
-      <ul>
-        {volume.books.map((book) => (
-          <li key={book.ordinal}>
-            {book.ordinal}: {book.title}
-          </li>
-        ))}
-      </ul>
-      <Image
-        src={volume.cover}
-        height={230}
-        width={140}
-        alt="Cover image of the volume"
-      />
+      <StyledHighlightBox $color={volume.color}>
+        <ul>
+          {volume.books.map((book) => (
+            <li key={book.ordinal}>
+              {book.ordinal}: {book.title}
+            </li>
+          ))}
+        </ul>
+        <Image
+          src={volume.cover}
+          height={230}
+          width={140}
+          alt="Cover image of the volume"
+        />
+      </StyledHighlightBox>
     </>
   );
 }
+
+const StyledHighlightBox = styled.div`
+  background-color: ${({ $color }) => $color};
+  display: flex;
+  padding: 20px;
+  justify-content: space-between;
+  align-items: center;
+`;
